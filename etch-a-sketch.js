c4d50d3be673400abf04cbeea4ccc6e84aac3289
nicholcr <nicholascrumb@yahoo.com>
2025-08-01 14:23:16 -0400

    created prompt to modify size of etch-a-sketch based on user input

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,20 @@
 const parentDiv = document.querySelector(".container");
-const arraySize = 100;
+let arraySize = 16;
+
+const btn = document.querySelector(".btnSize");
+btn.addEventListener('click', (event) => {
+    const userInput = prompt('Please enter a number between 1 and 100: ');
+    if (userInput < 0 || userInput > 100) {
+        //do nothing
+    } else {
+        arraySize = userInput;
+        createGrid();
+    }
+    changeSize();
+});
 
 function createGrid() {
+    parentDiv.replaceChildren();
     for (let i = 0; i < arraySize; i++) {
         const rowDiv = document.createElement("div");
         rowDiv.style.display = "flex";
@@ -11,7 +24,6 @@ function createGrid() {
         for (let j = 0; j < arraySize; j++) {
             const gridElement = document.createElement("div");
             gridElement.style.display = "flex";
-            gridElement.style.border = "1px solid";
             gridElement.style.flex = "1";
             gridElement.addEventListener('mouseover', (event) =>  {
                 gridElement.style.background = "black";
