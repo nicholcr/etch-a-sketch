bcaa2ba67560b0deb6b114f94c6b37fe8714b57b
nicholcr <nicholascrumb@yahoo.com>
2025-08-01 11:36:23 -0400

    modified script and style to allow the grid to take up the entire viewport

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,18 +1,21 @@
 const parentDiv = document.querySelector(".container");
-const arraySize = 16;
+const arraySize = 100;
 
 function createGrid() {
     for (let i = 0; i < arraySize; i++) {
         const rowDiv = document.createElement("div");
         rowDiv.style.display = "flex";
         rowDiv.style.flexDirection = "column";
+        rowDiv.style.width = "100vw";
 
         for (let j = 0; j < arraySize; j++) {
             const gridElement = document.createElement("div");
             gridElement.style.display = "flex";
-            gridElement.style.border = "2px solid";
-            gridElement.style.height = "30px";
-            gridElement.style.width = "30px";
+            gridElement.style.border = "1px solid";
+            gridElement.style.flex = "1";
+            gridElement.addEventListener('mouseover', (event) =>  {
+                gridElement.style.background = "black";
+            });
             rowDiv.appendChild(gridElement);
         }
 
